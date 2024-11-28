ed5e3a7dbe31c58e45067e9da9fbe56cb03151ef
Ahmad-U19 <ahmedoppal2592@gmail.com>
2024-11-28 21:14:01 +0500

    Final Code

---
+++ script.js
@@ -0,0 +1,27 @@
+var x=["Zohaib ","Hassan ","Yasir ","Ahmad "];
+
+document.write(x + "<br><br>");
+
+x.sort();
+
+document.write(x + "<br><br>");
+
+x.reverse();
+
+document.write(x + "<br><br>");
+
+x.pop();
+
+document.write(x + "<br><br>");
+
+x.push("Aswad");
+
+document.write(x + "<br><br>");
+
+x.shift();
+
+document.write(x + "<br><br>");
+
+x.unshift("Zawar");
+
+document.write(x + "<br><br>");
